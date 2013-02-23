501060a30080c7538b888632f7ff2786852a9428
alexandre <alexandre@PC.Alex>
2013-02-23 15:06:03 +0100

    Implémentation partielle de HttpResponse Il manque la classe "Page" et l'implémentation de la méthode "redirect404"

--- lib/core/HttpResponse.class.php
+++ lib/core/HttpResponse.class.php
@@ -9,7 +9,74 @@ namespace lib\core;
  */
 class HttpResponse
 {
-    //put your code here
+    /**
+     * 
+     * @var \lib\core\Page La page en réponse à la requête initiale
+     */
+    protected $page;
+
+    /**
+     * Permet d'ajouter un header à la page
+     * @param string $header Le header à ajouter
+     */
+    public function addHeader($header)
+    {
+        header($header);
+    }
+
+    /**
+     * Permet de renvoyer vers une autre URL
+     * @param string $location L’URL de redirection
+     */
+    public function redirect($location)
+    {
+        header('Location: ' . $location);
+        exit;
+    }
+
+    /**
+     * Permet de renvoyer vers une page d'erreur 404
+     * @todo Implémenter la méthode pour page 404
+     */
+    public function redirect404()
+    {
+        throw new \Exception;
+    }
+
+    /**
+     * Envoie la page au client web en réponse de sa requête
+     */
+    public function send()
+    {
+        exit($this->page->getGeneratedPage());
+    }
+
+    /**
+     * Permet d'affecter une page à l’attribut local page
+     * @param \lib\core\Page $page La Page à affecter
+     */
+    public function setPage(Page $page)
+    {
+        $this->page = $page;
+    }
+
+    /**
+     * Permet de créer un cookie
+     * Remarque : par défaut httpOnly est à true (contrairement à la méthode standard setcookie())
+     * @param string $name
+     * @param string $value
+     * @param int $expire
+     * @param string $path
+     * @param type $domain
+     * @param type $secure
+     * @param bool $httpOnly
+     */
+    public function setCookie($name, $value = '', $expire = 0, $path = null,
+                              $domain = null, $secure = false, $httpOnly = true)
+    {
+        setcookie($name, $value, $expire, $path, $domain, $secure, $httpOnly);
+    }
+
 }
 
 ?>
