f52388b39303a92224c4630b7cd8fc53df62df7c
alexandre <alexandre@PC.Alex>
2013-02-23 15:08:03 +0100

    Création du squelette de la classe Application et de son test unitaire

---
+++ lib/core/Application.class.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace lib\core;
+
+/**
+ * Classe représentant l’application Web.
+ * Assure la cohésion entre les différents composants de l’application 
+ * pour obtenir la réponse souhaitée par l’utilisateur en fonction de l’url entrée.
+ * @author M. l’Arbitre
+ */
+class Application
+{
+    //put your code here
+}
+
+?>
