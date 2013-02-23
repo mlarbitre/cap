731c46e03abfb76349ef76d53f2d724fbb5ca6d4
alexandre <alexandre@PC.Alex>
2013-02-23 17:50:05 +0100

    Ajout de tests unitaires sur ApplicationComponent

--- lib/core/ApplicationComponent.class.php
+++ lib/core/ApplicationComponent.class.php
@@ -8,7 +8,31 @@ namespace lib\core;
  */
 abstract class ApplicationComponent
 {
-    //put your code here
+
+    /**
+     * L'application dont dépend l'ApplicationComponent
+     * @var \lib\core\Application
+     */
+    protected $app;
+
+    /**
+     * Constructeur d’instance
+     * @param \lib\core\Application $app L'application dont dépend le composant
+     */
+    public function __construct(Application $app)
+    {
+        $this->app = $app;
+    }
+
+    /**
+     * Getter de l'application
+     * @return \lib\core\Application L'application dont dépend le composant
+     */
+    public function app()
+    {
+        return $this->app;
+    }
+
 }
 
 ?>
