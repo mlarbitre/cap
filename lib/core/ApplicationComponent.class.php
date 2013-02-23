6f3254e793e7fedb02ad196b1be024108f729b5b
alexandre <alexandre@PC.Alex>
2013-02-23 15:27:36 +0100

    Création du squelette de la classe ApplicationComponent et de son test unitaire

---
+++ lib/core/ApplicationComponent.class.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace lib\core;
+
+/**
+ * Classe abstraite représentant un composant d'une application
+ * @author M. l’Arbitre
+ */
+abstract class ApplicationComponent
+{
+    //put your code here
+}
+
+?>
