a908bf175ac06c7bf405498be55e1e2fde6b9123
alexandre <alexandre@PC.Alex>
2013-02-23 14:53:49 +0100

    Création du squelette de la classe HttpResponse et de son test unitaire

---
+++ lib/core/HttpResponse.class.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace lib\core;
+
+/**
+ * Classe représentant la réponse du serveur à la requête d’entrée.
+ * Permet la redirection, l’ajout de "header" et l’envoi d’une page html en réponse.
+ * @author alexandre
+ */
+class HttpResponse
+{
+    //put your code here
+}
+
+?>
